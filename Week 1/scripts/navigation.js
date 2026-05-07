4cec3326493d58523a4ecd9398d673ecdd7b9fba
seraquino <saquinonaranjo@byupathway.edu>
2026-05-06 20:54:17 -0600

    nice

--- Week 1/scripts/navigation.js
+++ Week 1/scripts/navigation.js
@@ -1,4 +1,8 @@
 const navbutton = document.getElementById('#ham-btn');
+const navBar = document.querySelector('#nav-bar');
+
+
 navbutton.addEventListener('click', () => {
     navbutton.classList.toggle('show');
+    navBar.classList.toggle('show');  
 });
