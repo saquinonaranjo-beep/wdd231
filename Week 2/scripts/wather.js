d0c0d56e3c2b15f976cdcdd2079403cb9de92328
seraquino <saquinonaranjo@byupathway.edu>
2026-05-12 15:00:53 -0600

    part3

--- Week 2/scripts/wather.js
+++ Week 2/scripts/wather.js
@@ -28,42 +28,16 @@ async function apiFetch() {
   }
 }
 
-const currentTemp = document.querySelector('#current-temp');
-const weatherIcon = document.querySelector('#weather-icon');
-const captionDesc = document.querySelector('figcaption');
-
-
-
-const myKey = "96323c663a757593af66b61ade97f128"
-const myLat = "49.75"
-const myLong = "6.64"
-
-
-
-const url = `https://api.openweathermap.org/data/2.5/weather?lat=${myLat}&lon=${myLong}&appid=${myKey}&units=imperial`;
-
-
-async function apiFetch() {
-  try {
-    const response = await fetch(url);
-    if (response.ok) {
-      const data = await response.json();
-      console.log(data); // testing only
-      displayResults(data); // uncomment when ready
-    } else {
-        throw Error(await response.text());
-    }
-  } catch (error) {
-      console.log(error);
-  }
-}
-
 function displayResults(data) {
-  currentTemp.innerHTML = `${data._____}&deg;F`;
-  const iconsrc = `https://openweathermap.org/img/w/${______}.___`;
-  let desc = data.weather[0].______;
-  weatherIcon.setAttribute('___', _____);
-  weatherIcon.setAttribute('___', _____);
+  currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+
+  const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+
+  let desc = data.weather[0].description;
+
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
+
   captionDesc.textContent = `${desc}`;
 }
 
