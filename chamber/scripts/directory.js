896a3c409d240f8032d7409598e1af1e6daff7d3
seraquino <saquinonaranjo@byupathway.edu>
2026-05-16 17:11:07 -0600

    buttons

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,25 +1,49 @@
+const gridButton = document.querySelector("#grid-view");
+const listButton = document.querySelector("#list-view");
+
 const container = document.querySelector("#members-container");
 const url = "data/members.json";
 
 async function getMembers() {
+
     const response = await fetch(url);
     const data = await response.json();
 
     data.forEach((member) => {
+
         console.log(member.name);
-  
+
         const card = document.createElement("div");
+
         card.classList.add("member-card");
+
         card.innerHTML = `
             <h3>${member.name}</h3>
             <p>${member.address}</p>
             <p>${member.phone}</p>
-            <p><a href="${member.website}" target="_blank" rel="noopener noreferrer">Visit Website</a></p>
+
+            <p>
+                <a href="${member.website}" 
+                   target="_blank" 
+                   rel="noopener noreferrer">
+                   Visit Website
+                </a>
+            </p>
+
             <img src="${member.image}" alt="${member.name}">
         `;
-        container.appendChild(card);
-});
 
+        container.appendChild(card);
+
+    });
 }
+
 getMembers();
 
+gridButton.addEventListener("click", () => {
+    console.log("Grid clicked");
+});
+
+listButton.addEventListener("click", () => {
+    console.log("List clicked");
+});
