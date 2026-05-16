7729e57d5c1e8affdda457a415863752c281491a
seraquino <saquinonaranjo@byupathway.edu>
2026-05-16 16:31:18 -0600

    add joson to the html

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,11 +1,25 @@
+const container = document.querySelector("#members-container");
 const url = "data/members.json";
 
 async function getMembers() {
     const response = await fetch(url);
     const data = await response.json();
 
-    console.log(data);
+    data.forEach((member) => {
+        console.log(member.name);
+  
+        const card = document.createElement("div");
+        card.classList.add("member-card");
+        card.innerHTML = `
+            <h3>${member.name}</h3>
+            <p>${member.address}</p>
+            <p>${member.phone}</p>
+            <p>${member.website}</p>
+            <img src="${member.image}" alt="${member.name}">
+        `;
+        container.appendChild(card);
+});
+
 }
-
-
 getMembers();
+
