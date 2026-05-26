6880587ee8cc2256d32e18988c387c4d30e62b10
seraquino <saquinonaranjo@byupathway.edu>
2026-05-25 21:09:30 -0600

    3

--- startLabTemple/js/scripts.js
+++ startLabTemple/js/scripts.js
@@ -3,3 +3,27 @@ console.log(temples);
 
 import {url} from '../data/temples.js';
 console.log(url);
+
+const showHere = document.querySelector("#showHere");
+
+const mydialog = document.querySelector("#mydialog");
+const mytitle = document.querySelector("#mytitle");
+const myinfo = document.querySelector("#mydialog p");
+const myclose = document.querySelector("#mydialog button");
+myclose.addEventListener("click", () => mydialog.close())
+
+
+function displayItems(data) {
+    console.log(data);
+    data.forEach(x => {
+        console.log(x)
+        const photo = document.createElement("img");
+        photo.src=`${url}${x.path}`
+        photo.alt = x.name
+        showHere.appendChild(photo);
+    });
+}
+
+
+displayItems(temples);
+
