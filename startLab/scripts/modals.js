c47b467eb7f5ebdfaca6ba27937e255713ffefc4
seraquino <saquinonaranjo@byupathway.edu>
2026-05-25 20:06:27 -0600

    comida

--- startLab/scripts/modals.js
+++ startLab/scripts/modals.js
@@ -1,8 +1,24 @@
-const openButton = document.querySelector("#openButton")
+const openButton1 = document.querySelector("#openButton1")
+const openButton2 = document.querySelector("#openButton2")
+const openButton3 = document.querySelector("#openButton3")
+
+
 const dialogBox = document.querySelector("#dialogBox")
+const dialogBoxText = document.querySelector("#dialogBox div")
 const closeButton = document.querySelector("#closeButton")
 
-openButton.addEventListener("click", () => {
+openButton1.addEventListener("click", () => {
+  dialogBoxText.innerHTML= "An apple has about 95 calories." 
+  dialogBox.showModal();
+});
+
+openButton2.addEventListener("click", () => {
+  dialogBoxText.innerHTML= "An orange has about 62 calories." 
+  dialogBox.showModal();
+});
+
+openButton3.addEventListener("click", () => {
+  dialogBoxText.innerHTML= "A banana has about 105 calories." 
   dialogBox.showModal();
 });
 
