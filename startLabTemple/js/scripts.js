d04ff7855201b3a8da771c52f2237ab6d20bf6b3
seraquino <saquinonaranjo@byupathway.edu>
2026-05-25 21:34:13 -0600

    done

--- startLabTemple/js/scripts.js
+++ startLabTemple/js/scripts.js
@@ -1,34 +1,58 @@
-import {temples} from '../data/temples.js';
-console.log(temples);
+<!DOCTYPE html>
+<html lang="en">
 
-import {url} from '../data/temples.js';
-console.log(url);
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Web Lab Start File">
+  <meta name="author" content="Paul Cheney">
 
-const showHere = document.querySelector("#showHere");
+  <link rel="stylesheet" href="styles/small.css">
+  <link rel="stylesheet" href="styles/large.css">
 
-const mydialog = document.querySelector("#mydialog");
-const mytitle = document.querySelector("#mytitle");
-const myinfo = document.querySelector("#mydialog p");
-const myclose = document.querySelector("#mydialog button");
-myclose.addEventListener("click", () => mydialog.close())
+  <title>Temple Gallery</title>
+</head>
 
+<body>
 
-function displayItems(data) {
-    console.log(data);
-    data.forEach(x => {
-        console.log(x)
-        const photo = document.createElement("img");
-        photo.src=`${url}${x.path}`
-        photo.alt = x.name
-        showHere.appendChild(photo);
-    });
-}
+  <header>
+    <span>Temple Gallery</span>
+  </header>
 
+  <nav>
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Temples</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+  </nav>
 
-displayItems(temples);
+  <main>
 
-function showStuff(x){
-    mytitle.textContent = x.name;
-    mydialog.showHere()
-    mydialog.showModal()
-}
+    <h1>Temple Gallery</h1>
+
+    <!-- Images appear here -->
+    <div id="showHere"></div>
+
+    <!-- Dialog Modal -->
+    <dialog id="mydialog">
+
+      <div class="dialog-top">
+        <h2 id="mytitle">Temple Title</h2>
+        <button class="close-btn">X</button>
+      </div>
+
+      <p></p>
+
+    </dialog>
+
+  </main>
+
+  <footer>
+    <p>&copy; Web Labs</p>
+  </footer>
+
+  <script src="js/scripts.js" type="module"></script>
+
+</body>
+</html>
