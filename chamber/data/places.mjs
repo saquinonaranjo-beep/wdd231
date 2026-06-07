d39a0e28df952026fbfd6aa061fe10d7df77b922
seraquino <saquinonaranjo@byupathway.edu>
2026-06-06 22:35:28 -0600

    nice

--- chamber/data/places.mjs
+++ chamber/data/places.mjs
@@ -2,7 +2,7 @@
 export const places = [
   {
     "name": "Aquarium del Puerto de Veracruz",
-    "image": "images/",
+    "image": "images/aquarium.webp",
     "address": "Blvd. Manuel Ávila Camacho s/n, Ricardo Flores Magón, Veracruz, Mexico",
     "description": "One of the largest aquariums in Latin America, featuring marine life from the Gulf of Mexico and beyond.",
     "link": "https://www.aquariumpuertodeveracruz.mx/"
